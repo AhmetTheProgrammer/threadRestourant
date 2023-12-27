5507aca892e76d8ece208ab1d16861910ec00090
GuldenCetin <guldencet54@gmail.com>
2023-12-27 20:42:04 +0300

    problem2 için gui ile bir şeyler yapıldı

--- src/GirisSayfasi.java
+++ src/GirisSayfasi.java
@@ -40,7 +40,8 @@ public class GirisSayfasi extends JFrame {
         problem2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+             Problem2 problem2=new Problem2();
+             problem2.setVisible(true);
             }
 
         });
